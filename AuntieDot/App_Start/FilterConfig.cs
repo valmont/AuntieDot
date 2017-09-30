11c6a834853e8065ec6fdcbcb7d689f6489e04c3
paul barton <paulb33@gmail.com>
2017-09-30 15:15:02 -0700

    init load

---
+++ AuntieDot/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AuntieDot {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
