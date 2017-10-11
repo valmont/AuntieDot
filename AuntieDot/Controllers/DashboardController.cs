315d5fa45233c4a55fc2de9da2889fa85c3faf93
paul barton <paulb33@gmail.com>
2017-10-10 19:57:11 -0700

    added dashboard data

--- AuntieDot/Controllers/DashboardController.cs
+++ AuntieDot/Controllers/DashboardController.cs
@@ -1,12 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 using AuntieDot.Attributes;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace AuntieDot.Controllers {
     [RequireSubscription]
     public class DashboardController : Controller {
         // GET: Dashboard
-        public ActionResult Index() {
-            return View();
+        public async Task<ActionResult> Index() {
+            var owinContext = HttpContext.GetOwinContext();
+            var usermanager = owinContext.GetUserManager<ApplicationUserManager>();
+            var user = await usermanager.FindByNameAsync(User.Identity.Name);
+            if (user == null) {
+                return new HttpUnauthorizedResult();
+            }
+            return View(user);
         }
     }
 }
