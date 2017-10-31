e0f0816d1cf6779894044bb9c84ff7deeb9a7435
paul barton <paulb33@gmail.com>
2017-10-30 21:03:51 -0700

    allow iframe

--- AuntieDot/Global.asax.cs
+++ AuntieDot/Global.asax.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace AuntieDot
-{
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
+namespace AuntieDot {
+    public class MvcApplication : HttpApplication {
+        protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_PreSendRequestHeaders() {
+            Response.Headers.Remove("X-Frame-Options");
         }
     }
 }
